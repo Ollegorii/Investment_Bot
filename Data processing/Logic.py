ac8348c47eb9c27e8b85d3fccf9e57df2a34c157
Ollegorii <remizov2003@gmail.com>
2022-05-12 02:14:33 +0300

    Logic
    Connected tg and tinkoff

--- Data processing/Logic.py
+++ Data processing/Logic.py
@@ -16,6 +16,9 @@ class Logic:
         self._tasks: List[asyncio.Task] = []
 
     def print_ui(self, chat_id: int, mes: str):
+        """
+        Добавляет в очередь сообщения, которые нужно вывести
+        """
         self.__UI_queue.put_nowait([chat_id, mes])
 
     async def registration(self, chat_id: int):
@@ -43,11 +46,11 @@ class Logic:
         try:
             users_to_loc = self.__users.set_index(['user_id'])
             token = users_to_loc.loc[chat_id].token
-            print(token)
             with Client(token) as client:
                 us = User(token, client)
                 portfolio = us.get_portfolio(account_id=us.get_account_id())
                 self.print_ui(chat_id, portfolio)
+
         except:
             self.print_ui(chat_id, "Токен оказался недействительным")
 
@@ -75,6 +78,26 @@ class Logic:
             self.print_ui(chat_id, "Figi оказался недействительным")
 
 
+    async def sell(self, chat_id: int):
+        """
+        Покупаем инвестиционную бумагу по figi пользователю с токеном,
+        найденном в Users.csv по chat_id
+        """
+        self.print_ui(chat_id, "Укажи figi бумаги, которую хочешь продать\n")
+        upd = await self.queue.get()
+        figi = upd.message.text
+        self.print_ui(chat_id, f"Укажи количество лотов с figi: {figi}, которое ты хочешь продать")
+        upd = await self.queue.get()
+        amount = upd.message.text
+        users_to_loc = self.__users.set_index(['user_id'])
+        token = users_to_loc.loc[chat_id].token
+        try:
+            # Функция Богдана с учетом проверки корректности figi
+            self.print_ui(chat_id, "Лоты успешно проданы, можешь проверять портфолио")
+        except:
+            self.print_ui(chat_id, "Figi оказался недействительным")
+
+
     async def plot(self, chat_id: int):
         """
         Выводим график бумаги по figi пользователю с токеном,
@@ -91,7 +114,11 @@ class Logic:
         except:
             self.print_ui(chat_id, "Figi оказался недействительным")
 
+
     async def distribution(self, upd: UpdateObj):
+        """
+         Вызывает функции в зависимости от поступившей команды
+        """
         mes = upd.message.text
         chat_id = upd.message.chat.id
         if mes == '/start':
@@ -105,13 +132,17 @@ class Logic:
             await self.buy_paper(chat_id)
         elif mes == "/plot":
             await self.plot(chat_id)
+        elif mes == "/sell":
+            await self.sell(chat_id)
         else:
             self.print_ui(chat_id, "Попробуй воспользоваться функциями из меню")
 
     async def _worker(self):
+        """
+        Рабочий цикл, проверяющий обновление в очереди пришедших сообщений
+        """
         while True:
             upd = await self.queue.get()
-            chat_id = upd.message.chat.id
             #print(upd)
             try:
                 await self.distribution(upd)
@@ -119,17 +150,29 @@ class Logic:
                 self.queue.task_done()
 
     async def start(self):
+        """
+        Ассинхронный запуск цикла worker
+        """
         self._tasks = [asyncio.create_task(self._worker()) for _ in range(self.concurrent_workers)]
 
     async def stop(self):
+        """
+        Выключение ассинхронных задач
+        """
         await self.queue.join()
         for t in self._tasks:
             t.cancel()
 
     async def add_token(self, user_id: int, token=0):
+        """
+        Дабавление токена по id пользователя из мессенджера
+        """
         self.__users = self.__users.append({'user_id': user_id, 'token': token}, ignore_index=True)
         print(self.__users)
         # self.__users.to_csv('Users.csv')
 
     async def find_id(self, user_id: int):
+        """
+        Проверка присутствия пользователя с данным user_id в pd data frame Users.csv
+        """
         return any(self.__users.user_id == user_id)
