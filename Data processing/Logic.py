c6cd46701a30d01ad03494e42e88c8a866a6893e
Ollegorii <remizov2003@gmail.com>
2022-05-12 00:00:42 +0300

    for Bogdan

--- Data processing/Logic.py
+++ Data processing/Logic.py
@@ -15,32 +15,91 @@ class Logic:
         self.concurrent_workers = concurrent_workers
         self._tasks: List[asyncio.Task] = []
 
-    # async def print(self, chat_id: int, txt: str):
-    #     await self.__tg_user.send_message(chat_id, txt)
+    def print_ui(self, chat_id: int, mes: str):
+        self.__UI_queue.put_nowait([chat_id, mes])
 
     async def registration(self, chat_id: int):
+        """
+        Регистрация пользователя в боте. Просим у пользователя его токен
+        в Инвестициях и записываем его в pandas data frame Users.csv.
+        Таким образом сопоставляем chat_id и токен в Инвестициях.
+        """
         if await self.find_id(chat_id):
-            self.__UI_queue.put_nowait([chat_id, "Ты уже зарегистрирован!"])
-        #   await Controller.print_ui(chat_id, "Ты уже зарегистрирован!")
-        #   await self.__tg_user.send_message(chat_id, "Ты уже зарегистрирован!")
+            self.print_ui(chat_id, "Ты уже зарегистрирован!")
             return
-        self.__UI_queue.put_nowait([chat_id, "Введи свой инвестиционный токен"])
+        self.print_ui(chat_id, "Введи свой инвестиционный токен")
         upd = await self.queue.get()
         while True:
             token = upd.message.text
             await self.add_token(chat_id, token)
-            self.__UI_queue.put_nowait([chat_id, "Записал😉"])
+            self.print_ui(chat_id, "Записал😉")
             return
+
+    async def get_portfolio(self, chat_id: int):
+        """
+        Показываем пользователю его инвестиционное портфолио по токену,
+        найденному в Users.csv по chat_id
+        """
+        try:
+            users_to_loc = self.__users.set_index(['user_id'])
+            token = users_to_loc.loc[chat_id].token
+            # portfolio = #Функция Богдана(token)
+            # self.print_ui(chat_id, portfolio)
+
+        except:
+            self.print_ui(chat_id, "Токен оказался недействительным")
+
+    async def buy_paper(self, chat_id: int):
+        """
+        Покупаем инвестиционную бумагу по figi пользователю с токеном,
+        найденном в Users.csv по chat_id
+        """
+        self.print_ui(chat_id, "Укажи figi бумаги, которую хочешь купить\n"
+                               "Figi - уникальный номер каждой бумаги, его можно узнать в интернете")
+        upd = await self.queue.get()
+        figi = upd.message.text
+        self.print_ui(chat_id, f"Укажи количество бумаг с figi: {figi}, которое ты хочешь купить")
+        users_to_loc = self.__users.set_index(['user_id'])
+        token = users_to_loc.loc[chat_id].token
+        try:
+            #Функция Богдана с учетом проверки корректности figi
+            self.print_ui(chat_id, "Бумага успешно куплена, можешь проверять порфтолио")
+        except:
+            self.print_ui(chat_id, "Figi оказался недействительным")
+
+
+    async def plot(self, chat_id: int):
+        """
+        Выводим график бумаги по figi пользователю с токеном,
+        найденном в Users.csv по chat_id
+        """
+        self.print_ui(chat_id, "Укажи figi бумаги, график которой ты хочешь видеть")
+        upd = await self.queue.get()
+        figi = upd.message.text
+        users_to_loc = self.__users.set_index(['user_id'])
+        token = users_to_loc.loc[chat_id].token
+        try:
+            pass
+            #Функция Богдана с учетом проверки корректности figi
+        except:
+            self.print_ui(chat_id, "Figi оказался недействительным")
 
     async def distribution(self, upd: UpdateObj):
         mes = upd.message.text
         chat_id = upd.message.chat.id
         if mes == '/start':
             await self.registration(chat_id)
-        elif mes != "":
-            if not await self.find_id(chat_id):
-                self.__UI_queue.put_nowait([chat_id, "Ты не зарегистрирован!\n"
-                                                         "Чтобы получить доступ к полному функционалу напиши /start и зарегистрируйся"])
+        elif mes != "" and not await self.find_id(chat_id):
+            self.print_ui(chat_id, "Ты не зарегистрирован!\n"
+                                   "Чтобы получить доступ к полному функционалу напиши /start и зарегистрируйся")
+        elif mes == "/portfolio":
+            await self.get_portfolio(chat_id)
+        elif mes == "/buy":
+            await self.buy_paper(chat_id)
+        elif mes == "/plot":
+            await self.plot(chat_id)
+        else:
+            self.print_ui(chat_id, "Попробуй воспользоваться функциями из меню")
 
     async def _worker(self):
         while True:
@@ -60,7 +119,7 @@ class Logic:
         for t in self._tasks:
             t.cancel()
 
-    async def add_token(self, user_id, token=0):
+    async def add_token(self, user_id: int, token=0):
         self.__users = self.__users.append({'user_id': user_id, 'token': token}, ignore_index=True)
         print(self.__users)
         # self.__users.to_csv('Users.csv')
--- Data processing/main.py
+++ Data processing/main.py
@@ -1,36 +1,30 @@
 from Logic import *
 from User import *
-from tinkoff.invest import Client, RequestError, PortfolioResponse, PositionsResponse, PortfolioPosition
+import pandas as pd
+import numpy as np
+import io
 import matplotlib.pyplot as plt
 SANDBOX_TOKEN = "t.Zx3k_Kx4AoPerZe11E7JRSyuWijNSQ-BClojho6m7PgGsUrVY1FVXk9ayjZV64egufFQZ_DLlpTmFilYPh4y7g"
 
-#b_id = broker_account_id = log.get_sync_client().register_sandbox_account(
-#            ti.SandboxRegisterRequest(broker_account_type=ti.BrokerAccountType.tinkoff)
-#        ).payload.broker_account_id
-#print(b_id)
-#print(stocks)
-#log.deposit_rub(100000)
-#tmp = c.get_market_bonds().payload
-#print(tmp)
-#log.buy(1, 'BBG00PNLY692')
-#log.sell()
-#print(c.get_portfolio().payload)
-#id = ti.get_broker_account_id()
-#print(id)
-with Client(SANDBOX_TOKEN) as client:
-    us = User(SANDBOX_TOKEN, client)
-    #us.deposit_usd(563.32)
-    #print(client.sandbox.get_sandbox_portfolio(account_id=us.get_account_id()))
-    #print(us.buy(us.get_account_id(), 'BBG004730ZJ9', 1))
-    #print(client.sandbox.get_sandbox_order_state(account_id=us.get_account_id(), order_id='31ea8257-da51-451d-92fa-c42a26744274'))
-    #us.sell(us.get_account_id(), 'BBG004730ZJ9', 1)
-    print(us.get_portfolio(us.get_account_id()))
-    print(us.get_orders(us.get_account_id()))
-    #print(client.instruments.shares())
-    print(us.search('BBG002BC7WC5'))
-    r = us.get_candels('BBG002BC7WC5').candles
-    df = us.create_df(r)
-    ax = df.plot(x='time', y='close')
-    plt.show()
-    #SPBXM
-    #BBG002BC7WC5
+# df = pd.DataFrame({
+#     'user_id': 845,
+#     'token': 34,
+# })
+# print(df)
+#users = pd.read_csv('Users.csv')
+#t="""user_id,token"""
+#users = pd.read_csv(io.StringIO(t))
+#users1 = users.set_index(['user_id'])
+users = pd.DataFrame({'user_id':[0], 'token':[0]})
+print(users)
+print(users)
+users = users.append({'user_id': 543, 'token': 546}, ignore_index=True)
+print(users)
+users = users.set_index(['user_id'])
+print(users.loc[543])
+#print(any(users.loc[543]))
+df = pd.DataFrame({'value':np.random.random(10000), 'key':range(100, 10100)})
+print(df.head())
+df_with_index = df.set_index(['key'])
+print(df_with_index.head())
+print(df_with_index.loc[10099])
